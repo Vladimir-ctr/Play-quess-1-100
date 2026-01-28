1f70802ad75e2d733b9a5ed286ede1fb46739179
Vladimir <morganspb4@gmail.com>
2026-01-28 17:13:35 +0300

    Добавил подсказки, вынес в отдельную функцию. Добавил функцию для расчета ращницы по модулю

--- main.go
+++ main.go
@@ -40,14 +40,36 @@ func main() {
 
 }
 
+// пишем дополнительную функцию abs для расчета разницы по модулю (т.е функция возвращает число всегда положительное)
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
 func hints(inputNumber int, randomNumber int) bool {
-	if inputNumber < randomNumber {
-		fmt.Println("Неверно, секретное число больше!")
-	} else if inputNumber > randomNumber {
-		fmt.Println("Неверно, секретное число меньше!")
-	} else if inputNumber == randomNumber {
-		fmt.Println("Ура!!! Вы угадали число!")
+	if inputNumber == randomNumber {
+		fmt.Println("Ура!!! Вы угадали секретное число")
 		return true
 	}
+
+	if inputNumber <= randomNumber {
+		fmt.Println("Секретное число больше")
+	} else {
+		fmt.Println("Секретное число меньше")
+	}
+
+	// создаем переменную в которой будем вычислять разницу между секретным числом и входным и передавать это число в функцию abs
+	different := abs(randomNumber - inputNumber)
+	// прописываем условия для подсказок
+	if different <= 5 {
+		fmt.Println("Горячо")
+	} else if different <= 15 {
+		fmt.Println("Тепло")
+	} else {
+		fmt.Println("Холодно")
+	}
 	return false
+
 }
